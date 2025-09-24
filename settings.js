a761dde338de2d986157f52cbc26d0b8c7dea299
Mastorihub <director.ankgroup@gmail.com>
2025-09-24 14:58:22 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "", //SUBZERO-MD;;;DUth3DxR#1DduAhUjiKBWfq2-PYgWkQb81k5H_Dv4rqshrM1P5b0 "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
 
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
+// CREATED BY YOUNG NDABA DE LILIAN
