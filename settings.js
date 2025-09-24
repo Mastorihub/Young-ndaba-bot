14c8f1559468cf7073a500aaf1455edbb1db51ae
Mastorihub <director.ankgroup@gmail.com>
2025-09-24 15:00:41 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", //SUBZERO-MD;;;DUth3DxR#1DduAhUjiKBWfq2-PYgWkQb81k5H_Dv4rqshrM1P5b0 "*"
+  SESSION_ID: "", //"SUBZERO-MD;;;DUth3DxR#1DduAhUjiKBWfq2-PYgWkQb81k5H_Dv4rqshrM1P5b0"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
